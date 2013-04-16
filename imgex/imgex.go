f91c6e049436946a495ffa2b822e924d0b738daa
Christopher Agocs <chris.agocs@gmail.com>
2013-04-15 20:34:53 -0400

    I figured out how to access pixels at the RGBA level!

--- imgex/imgex.go
+++ imgex/imgex.go
@@ -34,5 +34,27 @@ func main(){
 
 	fmt.Println("bounds: %v %v %v %v" , bounds.Min.X , bounds.Min.Y , bounds.Max.X, bounds.Max.Y)
 
+	for i := bounds.Min.Y; i <= bounds.Max.Y; i++ {
+		for j := bounds.Min.X; j <= bounds.Max.X; j++ {
+			r, g, b, _ := picture.At(j,i).RGBA()
+
+			gray := (r+g+b)/3
+
+			fmt.Printf("%v ", gray)
+		}
+		fmt.Print("\n")
+	}
+
+
 
 }
+
+
+
+//normalize takes an image, and returns the same image normalized such that the average value is 0x0FFF (in Go, colors go from 0x0000 to 0xFFFF).
+//I'm not sure what the best way to do this is, yet. I'm liking the algorithm described here: http://en.wikipedia.org/wiki/Normalization_(image_processing)
+//but I'm not sure if it will do what I want it to.
+//#TODO: Implement this method.
+// func normalize(picture image) image {
+// 	return picture
+// }
